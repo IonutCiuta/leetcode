989b32a349a8af11d99f457680dc55a01d77248c
Ionuț Ciuta <ionutciuta24@gmail.com>
2023-06-22 00:43:55 +0300

    Solution for `Problem 347` [Java☕️]

---
+++ P0347/Problem.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+class Solution {
+  public int[] topKFrequent(int[] nums, int k) {
+      var top = new HashMap<Integer, Integer>();
+      for (var n : nums) {
+          var freq = top.get(n);
+          top.put(n, freq == null ? 1 : (freq + 1));
+      }
+
+      var l = new ArrayList<>(top.entrySet());
+      l.sort((e1, e2) -> -Integer.compare(e1.getValue(), e2.getValue()));
+      
+      var r = new int[k];
+      for (var i = 0; i < k; i++) {
+          r[i] = l.get(i).getKey();
+      }
+      
+      return r;
+  }
+}
