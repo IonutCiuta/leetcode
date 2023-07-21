3e839df7885811258659b772d4bec5f848999071
Ionut Ciuta <ionutciuta24@gmail.com>
2023-07-21 10:14:08 +0300

    Solution for `Problem 153` [Kotlin 🔹]

--- P0153/Problem.kt
+++ P0153/Problem.kt
@@ -1,1 +1,25 @@
-// TODO
+class Solution {
+    fun findMin(nums: IntArray): Int {
+        var left = 0;
+        var right = nums.size - 1;
+        var min = 5000;
+
+        while (left <= right) {
+            
+            if (nums[left] < nums[right]) {
+                return if (nums[left] < min) nums[left] else min;
+            }
+
+            val mid = (left + right) / 2;
+            min = if (nums[mid] < min) nums[mid] else min;
+
+            if (nums[left] <= nums[mid]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return min;
+    }
+}
