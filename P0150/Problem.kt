62759c115d870238f76f3c400c4821abbc02ba43
Ionuț Ciuta <ionutciuta24@gmail.com>
2024-01-30 20:48:38 +0200

    Solution for `Problem 150` [ Kotlin 🔹]

--- P0150/Problem.kt
+++ P0150/Problem.kt
@@ -1,1 +1,25 @@
-// TODO
+import java.util.*
+
+class Solution {
+    fun evalRPN(tokens: Array<String>): Int {
+        val ops = Stack<Int>()
+        tokens.map {
+            when (it) {
+                "+" -> ops.push(ops.pop() + ops.pop())
+                "*" -> ops.push(ops.pop() * ops.pop())
+                "-" -> {
+                    val first = ops.pop()
+                    val second = ops.pop()
+                    ops.push(second - first)
+                }
+                "/" -> {
+                    val first = ops.pop()
+                    val second = ops.pop()
+                    ops.push(second / first)
+                }
+                else -> ops.push(it.toInt())
+            }
+        }
+        return ops.pop()
+    }
+}
