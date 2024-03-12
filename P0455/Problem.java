e3d42bbe2f90c342259437c9b53baadf1e557cad
Ionuț Ciuta <ionutciuta24@gmail.com>
2024-03-12 23:56:41 +0200

    Solution for `Problem 455` [ Java ☕️ ]

---
+++ P0455/Problem.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int r = 0;
+        for (int i = 0, j = 0; i < g.length && j < s.length;) {
+            if (g[i] <= s[j]) {
+                r++;
+                i++;
+            }
+            j++;
+        }
+        return r;
+    }
+}
