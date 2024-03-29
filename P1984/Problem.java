741e5e8433c6ed00251dfe9712ca54680d4ae0eb
Ionuț Ciuta <ionutciuta24@gmail.com>
2024-03-29 20:15:11 +0200

    Solution for `Problem 1984` [ Java ☕️ ]

---
+++ P1984/Problem.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minimumDifference(int[] nums, int k) {
+        if (k == 1) return 0;
+
+        Arrays.sort(nums);
+
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length - k + 1; i++) {
+            var diff = nums[i + k - 1] - nums[i];
+            min = Math.min(min, diff);
+        }
+
+        return min;
+    }
+}
